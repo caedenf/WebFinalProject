d46232c253d4d5aca3dd68b6f003d675b6e575c8
caedenf <42218750+caedenf@users.noreply.github.com>
2019-11-25 13:15:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,34 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var http = require("https");
-
-var options = {
-	"method": "GET",
-	"hostname": "deezerdevs-deezer.p.rapidapi.com",
-	"port": null,
-	"path": "/search?q=eminem",
-	"headers": {
-		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-		"x-rapidapi-key": "ed0387ed35msh68081b71c8d3a3cp129c22jsne333d255819e"
-	}
-};
-
-var req = http.request(options, function (res) {
-	var chunks = [];
-
-	res.on("data", function (chunk) {
-		chunks.push(chunk);
-	});
-
-	res.on("end", function () {
-		var body = Buffer.concat(chunks);
-		console.log(body.toString());
-	});
-});
-
-req.end();
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
