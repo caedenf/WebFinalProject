1858362c97b2fc7b43eccd5ca31c56421a8d8dd4
caedenf <42218750+caedenf@users.noreply.github.com>
2019-11-25 12:12:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 
 /* The following code is from Spotify */
 var request = require('request'); // "Request" library
@@ -147,3 +140,10 @@ app.get('/refresh_token', function(req, res) {
 });
 
 /* End of Spotify code */
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
