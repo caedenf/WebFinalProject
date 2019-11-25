ecb921c4e0c0bbec7cbd5dc7eb2599fbfb132cf7
caedenf <42218750+caedenf@users.noreply.github.com>
2019-11-25 12:15:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/views/pages'))
    .use(cors())
    .use(cookieParser());
 
