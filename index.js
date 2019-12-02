06ea77f4e0ce6c110a6f2c8906c2ac9c80516f28
caedenf <42218750+caedenf@users.noreply.github.com>
2019-12-02 14:08:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+var admin = require("firebase-admin");
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require("web-final-e7da6-firebase-adminsdk-1isyb-e48b74e854-1.json");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://web-final-e7da6.firebaseio.com"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
